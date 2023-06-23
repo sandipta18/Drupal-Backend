4bf33f50c0a7a45cd8185c8bb1d8942df9dad1d8
sandipta18 <sandipta.sardar@innoraft.com>
2023-06-23 11:08:54 +0530

    FT2023-340 : Created custom hook and explored predefined hooks .

---
+++ web/modules/custom/customhook/customhook.api.php
@@ -0,0 +1,18 @@
+<?php
+
+// This is a documentation of the customhook created for ticket FT2023-340
+// The purpose of this hook is to display the number of times a node is viewed
+// over a session
+
+/**
+ * @param int $current_count
+ * It defines the number of times user is viewing a node
+ * @param \Drupal\node\NodeInterface $node
+ * The node being viewed
+ */
+function hook_custom_hook_incremented($current_count, \Drupal\node\NodeInterface $node) {
+  if($current_count == 1) {
+    \Drupal::messenger()->addMessage('This is the first time you have viewed the node %title. ',['%title' => $node->label()]);
+  }
+}
+
