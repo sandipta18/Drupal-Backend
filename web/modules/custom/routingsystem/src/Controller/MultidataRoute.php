24efcabdb48478e17274028cfc06f452d50a8d48
sandipta18 <sandipta.sardar@innoraft.com>
2023-07-11 11:24:46 +0530

    FT2023-342 : Fixed code according to suuggestions of phpcs .

--- web/modules/custom/routingsystem/src/Controller/MultidataRoute.php
+++ web/modules/custom/routingsystem/src/Controller/MultidataRoute.php
@@ -5,22 +5,26 @@ namespace Drupal\routingsystem\Controller;
 use Drupal\Core\Controller\ControllerBase as ControllerControllerBase;
 
 /**
- * Controller for page route
+ * Controller for page route.
  */
 class MultidataRoute extends ControllerControllerBase {
 
-   /**
-    * @param mixed $data
-    *   Accepting dynamic parameter from url
-    *
-    * @return array
-    *   A render array to display the hello message
-    */
-   public function multiRoute($data) {
-      return [
-        '#type'   => 'markup',
-        '#markup' => t('Hi @value',[
-         '@value' => $data])
-      ];
-   }
+  /**
+   * Returns the overview page.
+   *
+   * @param mixed $data
+   *   Accepting dynamic parameter from url.
+   *
+   * @return array
+   *   A render array to display the hello message.
+   */
+  public function multiRoute($data) {
+    return [
+      '#type'   => 'markup',
+      '#markup' => t('Hi @value', [
+        '@value' => $data,
+      ]),
+    ];
+  }
+
 }
--- web/modules/custom/routingsystem/src/Controller/RoutingSystem.php
+++ web/modules/custom/routingsystem/src/Controller/RoutingSystem.php
@@ -3,74 +3,68 @@
 namespace Drupal\routingsystem\Controller;
 
 use Drupal\Core\Access\AccessResult;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Controller for page route
+ * Controller for page route.
  */
 class RoutingSystem extends ControllerBase {
 
   /**
    * Class property for current user.
    *
-   * @var \Drupal\Core\Session\AccountInterface;
+   * @var \Drupal\Core\Session\AccountInterface
    */
   protected $account;
 
   /**
-   * Constructs an Account Interface Object
+   * Constructs an Account Interface Object.
    *
-   * @param AccountInterface $account
-   *   Hold details about the account
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Hold details about the account.
    */
   public function __construct(AccountInterface $account) {
-     $this->account = $account;
+    $this->account = $account;
   }
 
   /**
-   * This function will be used to retrieve the data associated with the service
-   * with key 'current user'
-   *
-   * @param ContainerInterface $container
-   *   Instance of the dependancy injection container
-   *
-   * @return object
+   * {@inheritDoc}
    */
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('current_user')
     );
   }
+
   /**
-   * Prints the overview page
+   * Prints the overview page.
    *
    * @return array
-   *   Returns renderable array
+   *   Returns renderable array.
    */
   public function homeRoute() {
-    // Fetching the user name of the currently
+    // Fetching the user name of the currently.
     $userName = $this->account->getDisplayName();
     return [
       '#type'   => 'markup',
-      '#markup' => $this->t('Hello @user',[
-        '@user' =>$userName
-      ])
+      '#markup' => $this->t('Hello @user', [
+        '@user' => $userName,
+      ]),
     ];
   }
 
-
   /**
    * This function will be used to determine whether the access request should
-   * be accepted or denied
+   * be accepted or denied.
    *
    * @return object
-   *   Instance of the access result class
+   *   Instance of the access result class.
    */
-  public function customAccess () {
+  public function customAccess() {
     return ($this->account->hasPermission('routingsystem.permission')) ?
-    AccessResult::allowed() :  AccessResult::forbidden();
+    AccessResult::allowed() : AccessResult::forbidden();
   }
 
 }
